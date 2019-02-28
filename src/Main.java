2c4758d7d76fc618d4207b5c506b4aa593e5b320
SamGomes <sam.s.gomes@sapo.pt>
2019-02-28 19:45:41 +0000

    Added new project.

--- src/Main.java
+++ src/Main.java
@@ -1,52 +1,17 @@
-import java.io.File;
 import java.io.FileNotFoundException;
 import java.util.ArrayList;
-import java.util.HashSet;
-import java.util.Scanner;
 
 public class Main {
     public static void main(String[] args) {
 
         ArrayList<Photo> photos = new ArrayList<Photo>();
         try {
-            photos = readInputs("a_examples.txt");
+            photos = InputHelper.readInputs("a_examples.txt");
         } catch (FileNotFoundException e) {
             e.printStackTrace();
         }
 
         System.out.println(photos.toString());
-    }
-
-    public static ArrayList<Photo> readInputs(String filename) throws FileNotFoundException {
-
-        //read input
-        ArrayList<Photo> photos = new ArrayList<Photo>();
-        try {
-            Scanner inputScanner = new Scanner(new File(filename, filename + ".in"));
-
-
-            int idCounter = 0;
-            int numPhotos = inputScanner.nextInt();
-
-            //build slides
-            for (int i = 0; i < numPhotos; i++) {
-                String photoText = inputScanner.nextLine();
-                String[] photoAttributes = photoText.split(" ");
-                int numTags = Integer.parseInt(photoAttributes[1]);
-                HashSet<String> photoTags = new HashSet<String>();
-                for (int j = 0; j < numTags; j++) {
-                    photoTags.add(photoAttributes[j + 2]);
-                }
-                int orientation = (photoAttributes[0].equals("H")) ? 0 : 1;
-                photos.add(new Photo(idCounter++, orientation, photoTags));
-            }
-
-        } catch (FileNotFoundException e) {
-            System.out.println("file not found!");
-        }
-
-        return photos;
-
     }
 }
 
--- src/Slide.java
+++ src/Slide.java
@@ -16,25 +16,4 @@ public class Slide {
     public ArrayList<Photo> getPhotos() {
         return photos;
     }
-
-    public HashSet<String> getTags() {
-        return tags;
-    }
-
-    public int calculateInterest(Slide otherSlide) {
-
-        int result;
-
-        HashSet<String> commonTags = new HashSet<String>(this.getTags());
-        commonTags.retainAll(otherSlide.getTags());
-        int nCommonTags = commonTags.size();
-
-        int nTagsOnlyOnMe = this.getTags().size() - nCommonTags;
-        int nTagsOnlyOnOther = otherSlide.getTags().size() - nCommonTags;
-
-        result = Math.min(nCommonTags, nTagsOnlyOnMe);
-        result = Math.min(result, nTagsOnlyOnOther);
-
-        return result;
-    }
 }
